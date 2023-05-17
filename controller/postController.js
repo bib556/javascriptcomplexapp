02898e49b5aeac8f4c19ee00027cb5c799c3e686
bib556 <109883609+bib556@users.noreply.github.com>
2023-05-17 22:23:21 +0530

    second push of all files.

---
+++ controller/postController.js
@@ -0,0 +1,105 @@
+const Post = require("../models/Post")
+exports.viewCreateScreen = function(req,res){
+res.render("create-post")
+}
+
+exports.create = function(req,res){
+let post = new Post(req.body, req.session.user._id)
+post.create().then(function(newId){
+   req.flash("success", "new post created successfully")
+   req.session.save(()=> res.redirect(`/post/${newId}`))
+}).catch(function(errors){
+   errors.forEach(() => req.flash("errors", error))
+   req.session.save(() => res.redirect("/create-post"))
+})
+}
+
+exports.apiCreate = function(req,res){
+  let post = new Post(req.body, req.apiUser._id)
+  post.create().then(function(newId){
+   res.json("congrats .")
+  }).catch(function(errors){
+  res.json(errors)
+  })
+  }
+
+
+exports.viewSingle = async function(req,res){
+try{
+let post =  await Post.findSingleById(req.params.id, req.visitorId)
+res.render("single-post-screen",{post: post, title:post.title})
+}catch{
+res.render("404")
+}
+}
+
+exports.viewEditScreen = async function(req, res) {
+    try {
+      let post = await Post.findSingleById(req.params.id, req.visitorId)
+      if (post.isVisitorOwner) {
+        res.render("edit-post", {post: post})
+      } else {
+        req.flash("errors", "You do not have permission to perform that action.")
+        req.session.save(() => res.redirect("/"))
+      }
+    } catch {
+      res.render("404")
+    }
+  }
+
+exports.edit = function(req,res){
+    let post = new Post(req.body, req.visitorId ,req.params.id )
+    post.update().then((status) => {
+// the post was successfully updated in the database 
+//  or user did have permission but, there was a validation errors
+if(status == "success"){
+// post was updated in db 
+req.flash("success","post successfully updated.")
+req.session.save(function(){
+    res.redirect(`/post/${req.params.id}/edit`)
+})
+}else{
+post.errors.forEach(function(error){
+    req.flash("errors",error)
+})
+req.session.save(function(){
+    res.redirect(`/post/${req.params.id}/edit`)
+})
+}
+    }).catch(()=>{
+// a post with the requested id doesnt exist 
+// or if the current visitor is not the owner  of the requested post 
+req.flash ("errors","you do not have the permission to perform this action.")
+req.session.save(function(){
+    res.redirect("/")
+})
+    })
+}
+
+exports.delete = function(req,res){
+    Post.delete(req.params.id,req.visitorId).then(()=>{
+        console.log(req.params.id)
+        console.log(req.visitorId)
+    req.flash("success","post successfully deleted.")
+    req.session.save(()=> res.redirect(`/profile/${req.session.user.username}`))
+    }).catch(()=>{
+     req.flash("errors","you do not have permission to perform this action" )
+     req.session.save(()=>res.redirect("/") )
+    })
+}
+
+exports.apiDelete = function(req,res){
+  Post.delete(req.params.id,req.apiUser._id).then(()=>{
+     res.json("success")
+  }).catch(()=>{
+  res.json("you donot have permisssion to perform this action.")
+  })
+}
+
+exports.search = function(req,res){
+  Post.search(req.body.searchTerm).then((posts)=>{
+res.json(posts)
+  }).catch(()=>{
+res.json([])
+  })
+}
---
+++ models/User.js
@@ -0,0 +1,131 @@
+const bcrypt =require("bcryptjs")
+const usersCollection = require("../db").db().collection("users")
+const validator = require("validator")
+const md5 = require("md5")
+
+
+let User = function(data, getAvatar){
+this.data = data 
+this.errors =[]
+if(getAvatar == undefined){getAvatar = false}
+if(getAvatar){this.getAvatar()}
+}
+User.prototype.cleanUp = function(){
+    if(typeof(this.data.username)!="string"){  this.data.username ="" }
+    if(typeof(this.data.email)!="string"){  this.data.email ="" }
+    if(typeof(this.data.password)!="string"){  this.data.password ="" }
+    // get rid of bogus properties 
+    this.data = {
+      username: this.data.username.trim().toLowerCase(),
+      email:this.data.email.trim().toLowerCase(),
+      password:this.data.password  
+    }
+}
+User.prototype.validate = function(){
+  return new Promise(async (resolve,reject)=>{
+    if(this.data.username== ""){this.errors.push("you must provide a username .")}
+    if(this.data.username!="" && !validator.isAlphanumeric(this.data.username)){this.errors.push("username can only contain number and alphabets ")}
+    if(!validator.isEmail(this.data.email)){this.errors.push("you must provide a valid email .")}
+    if(this.data.password == ""){this.errors.push("you must provide a password .")}
+    if(this.data.password.length > 0 && this.data.password.length < 12){this.errors.push("the password length must be greater than 12 characters")}
+    if(this.data.password.length > 50){this.errors.push("password cannot exceed 50 char")}
+    if(this.data.username.length >0 && this.data.username.length < 3){this.errors.push("username must be atleast 3 characters .")}
+    if(this.data.username.length > 30){this.errors.push("username must exceed 30 character.")}
+    
+    //Only if username is valid then check to see if this is already taken 
+    if(this.data.username.length >2 && this.data.username.length <31 && validator.isAlphanumeric(this.data.username)){
+      let usernameExists=  await usersCollection.findOne({username: this.data.username})
+      if(usernameExists){this.errors.push("the username is already taken. ") }
+    
+    }
+    //Only if email is valid then check to see if this is already taken 
+    if(validator.isEmail(this.data.email)){
+      let emailExists=  await usersCollection.findOne({email: this.data.email})
+      if(emailExists){this.errors.push("the email is already taken .") }
+    
+    }
+    resolve()
+    })
+}
+
+User.prototype.login =  function(){
+return new Promise(async (resolve,reject) => {
+  this.cleanUp()
+const attemptedUser = await usersCollection.findOne({username:this.data.username})
+if(attemptedUser && bcrypt.compareSync(this.data.password,attemptedUser.password)){
+  this.data = attemptedUser
+  this.getAvatar()
+  resolve("congrats")
+}else{
+  reject("invalid username/ password")
+}
+
+})
+
+}
+
+User.prototype.register = function(){
+  return new Promise(async (resolve,reject)=>{
+    //step#1 validate user data  
+    this.cleanUp()  
+    await this.validate()
+    // step # only if there are no validation error 
+    // then save the user data into a database
+    if(!this.errors.length) {
+     // hash user password
+     let salt = bcrypt.genSaltSync(10)
+     this.data.password = bcrypt.hashSync(this.data.password,salt)
+     await usersCollection.insertOne(this.data)
+     this.getAvatar()
+     resolve()
+    }else{
+      reject(this.errors)
+    }
+    
+   })
+}
+
+User.prototype.getAvatar =function(){
+  this.avatar = ` https://gravatar.com/avatar/email/${md5(this.data.email)}?s=128`
+}
+
+User.findByUsername = function(username){
+  return new Promise(function(resolve,reject){
+    if(typeof(username) !="string"){
+      reject()
+      return
+    }
+    usersCollection.findOne({username:username}).then(function(userDoc){
+     if(userDoc){
+      userDoc = new User(userDoc,true)
+      userDoc ={
+       _id: userDoc.data._id,
+       username: userDoc.data.username,
+       avatar: userDoc.avatar
+      }
+      resolve(userDoc)
+     }else {
+      reject()
+     }
+    }).catch(function(){
+      reject()
+    })
+  })
+}
+
+User.doesEmailExist = function(email){
+  return  new Promise(async function(resolve, reject){
+    if(typeof(email) !="string"){
+      resolve(false)
+      return
+    }
+    let user = await usersCollection.findOne({email: email})
+    if(user){
+      resolve(true)
+    }else{
+      resolve(false)
+    }
+  })
+}
+
+module.exports = User
